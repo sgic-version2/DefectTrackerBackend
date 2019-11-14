8a7bca52c04ad15ea923b1c8dd326c7da7d00618
sasikumar44 <srikumarans9@gmail.com>
2019-11-14 11:16:24 +0530

    Login Service

---
+++ LoginService/src/main/java/com/Sgic/DefectTracker/LoginService/enums/RestApiResoponseStatus.java
@@ -0,0 +1,5 @@
+package com.Sgic.DefectTracker.LoginService.enums;
+
+public enum RestApiResoponseStatus {
+
+}
---
+++ LoginService/src/main/java/com/Sgic/DefectTracker/LoginService/util/Constatnts.java
@@ -0,0 +1,5 @@
+package com.Sgic.DefectTracker.LoginService.util;
+
+public class Constatnts {
+
+}
