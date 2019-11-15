cd59c371908b26a7ecee1278c02fd793652151ae
kirikaran <kirikarankiri63@gmail.com>
2019-11-15 14:44:06 +0530

    kiri resolved

--- ProductService/src/main/java/com/Sgic/DefectTracker/ProductService/entities/PrivilegeDetailsEntity.java
+++ ProductService/src/main/java/com/Sgic/DefectTracker/ProductService/entities/PrivilegeDetailsEntity.java
@@ -1,8 +1,14 @@
 package com.Sgic.DefectTracker.ProductService.entities;
 
+import javax.persistence.Entity;
 import javax.persistence.Table;
-
+@Entity
 @Table( schema="prodect_service",name= "privilege_details")
 public class PrivilegeDetailsEntity {
+private Long id;
+private Long role_id;
+private Long privilege_id;
+
+
 
 }
