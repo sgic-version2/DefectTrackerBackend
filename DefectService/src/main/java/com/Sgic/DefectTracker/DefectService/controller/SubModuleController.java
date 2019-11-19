af2a4ab6ba055edec134fe73f683e7a74ae1bee1
unknown <sasee9488@gmail.com>
2019-11-19 16:14:19 +0530

    update

--- DefectService/src/main/java/com/Sgic/DefectTracker/DefectService/controller/SubModuleController.java
+++ DefectService/src/main/java/com/Sgic/DefectTracker/DefectService/controller/SubModuleController.java
@@ -6,7 +6,6 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,12 +13,13 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.Sgic.DefectTracker.DefectService.entities.SubModule;
 import com.Sgic.DefectTracker.DefectService.services.SubModuleService;
 
-@Repository
-@RequestMapping("/ap/v1")
+@RestController
+@RequestMapping("/api/v1")
 public class SubModuleController {
 
 	@Autowired
--- DefectService/src/main/java/com/Sgic/DefectTracker/DefectService/entities/SubModule.java
+++ DefectService/src/main/java/com/Sgic/DefectTracker/DefectService/entities/SubModule.java
@@ -2,22 +2,37 @@ package com.Sgic.DefectTracker.DefectService.entities;
 
 import java.io.Serializable;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
 @Entity
-@Table(name="")
+@Table(name="submodule")
 public class SubModule implements Serializable{
 
 	private static final long serialVersionUID = 1L;
 	@Id
-	
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private long submodule_id;
 	private String submodule_name;
+	@ManyToOne(cascade = { CascadeType.MERGE, CascadeType.REMOVE })
+	@JoinColumn(name = "module_id")
+	@OnDelete(action = OnDeleteAction.CASCADE)
+	private Module module;
+	public Module getModule() {
+		return module;
+	}
+	public void setModule(Module module) {
+		this.module = module;
+	}
 	public long getSubmodule_id() {
 		return submodule_id;
 	}
