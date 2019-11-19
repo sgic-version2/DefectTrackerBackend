ca7c5fd806017765d55146d5dd1ae2d956f655b7
sasikumar44 <srikumarans9@gmail.com>
2019-11-19 16:50:52 +0530

    lastlyadded

--- EmployeeService/src/main/java/com/Sgic/DefectTracker/EmployeeService/controller/DesignationController.java
+++ EmployeeService/src/main/java/com/Sgic/DefectTracker/EmployeeService/controller/DesignationController.java
@@ -1,8 +1,13 @@
 package com.Sgic.DefectTracker.EmployeeService.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.Sgic.DefectTracker.EmployeeService.entities.Designation;
 import com.Sgic.DefectTracker.EmployeeService.service.DesignationService;
 
 @RestController
@@ -10,6 +15,15 @@ public class DesignationController {
 	
 	@Autowired
 	private DesignationService designationService;
+	
+	
+	@PostMapping("/designation")
+	public ResponseEntity<?> createNote(@RequestBody Designation designation)
+	{
+		designationService.createDesignation(designation);
+		
+		return new ResponseEntity<Object>(HttpStatus.OK);
+	}
 	
 	
 	
