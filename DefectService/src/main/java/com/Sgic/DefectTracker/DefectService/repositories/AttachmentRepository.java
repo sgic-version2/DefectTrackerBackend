428e364d6f53da22451e6960b3bf7619fb693214
jathurshan <s.jathur93@gmail.com>
2019-11-20 22:08:40 +0530

    file attachment for defects from branch jathurshan

--- DefectService/src/main/java/com/Sgic/DefectTracker/DefectService/repositories/AttachmentRepository.java
+++ DefectService/src/main/java/com/Sgic/DefectTracker/DefectService/repositories/AttachmentRepository.java
@@ -3,9 +3,9 @@ package com.Sgic.DefectTracker.DefectService.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.Sgic.DefectTracker.DefectService.entities.DefectStatus;
+import com.Sgic.DefectTracker.DefectService.entities.Attachment;
 
 @Repository
-public interface DefectStatusRepository extends JpaRepository<DefectStatus, Long> {
+public interface AttachmentRepository extends JpaRepository<Attachment, Long>{
 
 }
