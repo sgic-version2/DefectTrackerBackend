b5614f379d9b0b2bb42423c90072894096eb462d
Jathurshan <s.jathur93@gmail.com>
2019-12-06 15:27:45 +0530

    Adding DTO classes to project from branch jathurshan

---
+++ DefectService/src/main/java/com/Sgic/DefectTracker/DefectService/dto/ProjectQueryDTO.java
@@ -0,0 +1,45 @@
+package com.Sgic.DefectTracker.DefectService.dto;
+
+public class ProjectQueryDTO {
+	private long projectId;
+	private String project_name;
+	private String start_date;
+	private String end_date;
+	private String status;
+	private String project_type;
+
+	public ProjectQueryDTO(long projectId, String project_name, String start_date, String end_date, String status,
+			String project_type) {
+		this.projectId = projectId;
+		this.project_name = project_name;
+		this.start_date = start_date;
+		this.end_date = end_date;
+		this.status = status;
+		this.project_type = project_type;
+	}
+
+	public long getProjectId() {
+		return projectId;
+	}
+
+	public String getProject_name() {
+		return project_name;
+	}
+
+	public String getStart_date() {
+		return start_date;
+	}
+
+	public String getEnd_date() {
+		return end_date;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public String getProject_type() {
+		return project_type;
+	}
+
+}
--- DefectService/src/main/java/com/Sgic/DefectTracker/DefectService/services/ProjectServiceImplement.java
+++ DefectService/src/main/java/com/Sgic/DefectTracker/DefectService/services/ProjectServiceImplement.java
@@ -1,59 +1,110 @@
 package com.Sgic.DefectTracker.DefectService.services;
 
-
+import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.Sgic.DefectTracker.DefectService.dto.ProjectCreateDTO;
+import com.Sgic.DefectTracker.DefectService.dto.ProjectQueryDTO;
+import com.Sgic.DefectTracker.DefectService.dto.ProjectUpdateDTO;
 import com.Sgic.DefectTracker.DefectService.entities.Project;
 import com.Sgic.DefectTracker.DefectService.repositories.ProjectRepository;
 
 @Service
-public class ProjectServiceImplement  implements ProjectServices{
+public class ProjectServiceImplement implements ProjectServices {
 
 	@Autowired
 	private ProjectRepository projectRepository;
 
-	@Override
-	public Project saveProject(Project project) {
-		// TODO Auto-generated method stub
-		return projectRepository.save(project);
+//	@Override
+//	public Project saveProject(Project project) {
+//		// TODO Auto-generated method stub
+//		return projectRepository.save(project);
+//	}
+//
+//	
+//	@Override
+//	public Project updateProject(Project project, long id) {
+//		// TODO Auto-generated method stub
+//		return projectRepository.save(project);
+//	}
+//
+//	@Override
+//	public void deleteProject(long id) {
+//		// TODO Auto-generated method stub
+//		projectRepository.deleteById(id);
+//	}
+//	
+//	@Override
+//	public Optional<Project> findByID(long id) {
+//		// TODO Auto-generated method stub
+//		return projectRepository.findById(id);
+//	}
+//
+//
+//	@Override
+//	public List<Project> getAllProject() {
+//		// TODO Auto-generated method stub
+//		return projectRepository.findAll();
+//	}
+
+	public ProjectQueryDTO getProjectById(Long id) {
+		if (projectRepository.findById(id).isPresent()) {
+			Project fetchProject = projectRepository.findById(id).get();
+			return new ProjectQueryDTO(fetchProject.getProjectId(), fetchProject.getProject_name(),
+					fetchProject.getStart_date(), fetchProject.getEnd_date(), fetchProject.getStatus(),
+					fetchProject.getProject_type());
+		} else {
+			return null;
+		}
 	}
 
-	
-	@Override
-	public Project updateProject(Project project, long id) {
-		// TODO Auto-generated method stub
-		return projectRepository.save(project);
+	public List<Object> getAllProjects() {
+		List<Object> projectList = new ArrayList<>();
+		projectRepository.findAll().forEach(project -> {
+			projectList.add(new ProjectQueryDTO(project.getProjectId(), project.getProject_name(),
+					project.getStart_date(), project.getEnd_date(), project.getStatus(), project.getProject_type()));
+		});
+
+		return projectList;
 	}
 
-	@Override
-	public void deleteProject(long id) {
-		// TODO Auto-generated method stub
-		projectRepository.deleteById(id);
-	}
-	
-	@Override
-	public Optional<Project> findByID(long id) {
-		// TODO Auto-generated method stub
-		return projectRepository.findById(id);
-	}
+	public Long createProject(ProjectCreateDTO projectCreateDTO) {
+		Project newProject = new Project();
+		newProject.setProject_name(projectCreateDTO.getProject_name());
+		newProject.setStart_date(projectCreateDTO.getStart_date());
+		newProject.setEnd_date(projectCreateDTO.getEnd_date());
+		newProject.setStatus(projectCreateDTO.getStatus());
+		newProject.setProject_type(projectCreateDTO.getProject_type());
 
-
-	@Override
-	public List<Project> getAllProject() {
-		// TODO Auto-generated method stub
-		return projectRepository.findAll();
+		return projectRepository.save(newProject).getProjectId();
 	}
 
-	
-	
+	public ProjectQueryDTO updateProject(Long id, ProjectUpdateDTO projectUpdateDTO) {
+		if (projectRepository.findById(id).isPresent()) {
+			Project existingProject = projectRepository.findById(id).get();
 
+			existingProject.setProject_name(projectUpdateDTO.getProject_name());
+			existingProject.setEnd_date(projectUpdateDTO.getEnd_date());
+			existingProject.setStatus(projectUpdateDTO.getStatus());
+			existingProject.setProject_type(projectUpdateDTO.getProject_type());
+
+			Project updatedProject = projectRepository.save(existingProject);
+
+			return new ProjectQueryDTO(updatedProject.getProjectId(), updatedProject.getProject_name(),
+					updatedProject.getStart_date(), updatedProject.getEnd_date(), updatedProject.getStatus(),
+					updatedProject.getProject_type());
+		} else {
+			return null;
+		}
 	}
-	
-	
 
-	
+	public void deleteProject(Long id) {
+		if (projectRepository.findById(id).isPresent()) {
+			projectRepository.deleteById(id);
+		}
+	}
 
+}
